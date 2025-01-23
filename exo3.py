a861deb77e78db090c2dd3790de3326fedaa05c3
Rymzz <rym.zidi@polymtl.ca>
2025-01-23 12:15:34 -0500

    exo3

--- exo3.py
+++ exo3.py
@@ -4,42 +4,31 @@ a = int(input("Entrez a, non nul: "))
 b = int(input("Entrez b: "))
 c = int(input("Entrez c: "))
 
-# Calculer le discriminant et assigner la valeur dans la variable "delta"
-# delta = ...
+delta = (b**2 - a*c)
+#Définitions des cas possibles
+naPasDeSolution = delta < 0
+aUneSeuleSolution = delta == 0
+aDeuxSolutions = delta > 0
 
-# Déterminer la condition (bool) qui correspond à aucune solution de l'équation et mettre la valeur dans la variable "naPasDeSolution"
-# naPasDeSolution = ...
-
+naPasDeSolution = delta < 0
 if naPasDeSolution:
-    # ces ligne de code seront executé si il y'a aucune racine
-    # afficher sur l'écran "Aucune racine"
-    # print("Aucune racine")
-    pass
+    print("Aucune racine")
+
+
 else:
-    # Déterminer la condition (bool) qui correspond à une unique solution de l'équation et mettre la valeur dans "aUneSeuleSolution"
-    # aUneSeuleSolution = ...
-
-    if aUneSeuleSolution:
-        # ces ligne de code seront executé si il y'a une seule racine
-        # afficher sur l'écran "Une seule racine"
-        # print("Une seule racine")
-        # assigner a la variable x1 la valeur de la racine
-        # x1 = ...
-        # print(x1)
-        pass
-    else:
-        # Déterminer la condition (bool) qui correspond à deux solutions de l'équation et mettre la valeur dans "aDeuxSolutions"
-        # aDeuxSolutions = ...
-
-        if aDeuxSolutions:
-            # afficher sur l'écran "Deux racines"
-            # print("Deux racines")
-            # calculer la prmiere racine, assigner la a "x1"
-            # x1 = ...
-            # calculer la deuxieme racine, assigner la a "x2"
-            # x2 = ...
-            # print(x1, x2)
-            pass
+     aUneSeuleSolution = delta == 0
+     if aUneSeuleSolution:
+        x1 = float(-b/(2*a))
+        print("Une seule racine", )
+        print(x1)
+        
+     else: aDeuxSolutions = delta > 0
+     if aDeuxSolutions:
+             x1 = float((-b-(delta)**0.5)/(2*a))
+             x2 = float((-b+(delta)**0.5)/(2*a))
+             print("Deux racines")
+             print(x1, x2)
+             
 
 # Exemple d'utilisation:
 # Pour a = 1, b = -3, c = 2
