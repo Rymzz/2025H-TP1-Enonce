c2b8d9a7bd9c49bdd4eab4f60df5624aac55962a
Rymzz <rym.zidi@polymtl.ca>
2025-01-23 11:29:36 -0500

    exo1

--- exo1.py
+++ exo1.py
@@ -1,9 +1,11 @@
 # Demander le nom complet de l'utilisateur
 nom_complet = input("Veuillez entrer votre nom complet:")
 # Demander l'âge de l'utilisateur
-
+âge = int(input("Veuillez entrer votre âge:"))
 # Définir l'année actuelle
+année_actuelle = 2025
 # Calculer l'année de naissance
-
+année_naissance = (année_actuelle - âge)
+print("Bienvenue", (nom_complet), "!", "Vous êtes née en", (année_naissance), ".")
 # Afficher un message de bienvenue avec le nom complet
 # Afficher l'année de naissance
